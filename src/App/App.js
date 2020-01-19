0b933fcdf108d8475b8b9e9c3247a70c73309aa0
Ashley Claiborne <languageisaliquid@gmail.com>
2020-01-19 15:44:13 -0600

    setup complete

---
+++ src/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <buttton className='btn btn-light'>Sign in with Google</buttton>
+      </div>
+    );
+  }
+}
+
+export default App;
