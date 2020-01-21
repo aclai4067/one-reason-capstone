b168d446983c097e8988412c4b3477f0f1a383a5
Ashley Claiborne <languageisaliquid@gmail.com>
2020-01-20 21:57:00 -0600

    WIP

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,8 +1,24 @@
 import './Home.scss';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import userData from '../../../helpers/data/userData';
+import authData from '../../../helpers/data/authData';
 
 class Home extends React.Component {
+  state = {
+    user: {},
+  }
+
+  componentDidMount() {
+    const uid = authData.getUid();
+    userData.getUserByUid(uid)
+      .then((existingUser) => {
+        if (existingUser) {
+          this.setState({ user: existingUser });
+        }
+      }).catch((err) => console.error('error getting user by uid from Home', err));
+  }
+
   render() {
     const profileId = 12345;
     const feedId = 67890;
