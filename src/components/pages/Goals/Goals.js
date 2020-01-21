7ad2d78faf169e8261de48c5dd3be71254f04cbc
Ashley Claiborne <languageisaliquid@gmail.com>
2020-01-20 20:47:26 -0600

    all routes working

--- src/components/pages/Goals/Goals.js
+++ src/components/pages/Goals/Goals.js
@@ -1,11 +1,15 @@
 import './Goals.scss';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Goals extends React.Component {
   render() {
+    const goalId = 4067;
     return (
       <div className='Goals'>
         <h1>Goals</h1>
+        <Link className='btn btn-outline-dark' to='goals/new'>New Goal</Link>
+        <Link class='btn btn-outline-info' to={`/goals/${goalId}/edit`}>Edit Goal</Link>
       </div>
     );
   }
