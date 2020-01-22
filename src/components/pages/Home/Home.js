c7000b6bc90b79c52ccc37e35877afb7189e68c8
Ashley Claiborne <languageisaliquid@gmail.com>
2020-01-21 22:02:27 -0600

    profile details print and set states for feed

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -4,14 +4,34 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import userData from '../../../helpers/data/userData';
 import authData from '../../../helpers/data/authData';
+import goalData from '../../../helpers/data/goalData';
+import feedData from '../../../helpers/data/feedData';
 
 class Home extends React.Component {
   state = {
     user: {},
+    goalsMet: 0,
+    userFeed: [],
+    selectedUserFeed: [],
   }
 
   static propTypes = {
     setUser: PropTypes.func,
+  }
+
+  getGoalCount = (uid) => {
+    goalData.getGoalsByUid(uid)
+      .then((results) => {
+        const metGoals = results.filter((x) => x.isMet === true);
+        this.setState({ goalsMet: metGoals.length });
+      }).catch((err) => console.error('error from getGoalCount', err));
+  }
+
+  setUserFeed = (uid) => {
+    feedData.getFeedByUid(uid)
+      .then((results) => {
+        this.setState({ userFeed: results, selectedUserFeed: results });
+      }).catch((err) => console.error('error from getUserFeed', err));
   }
 
   componentDidMount() {
@@ -21,13 +41,15 @@ class Home extends React.Component {
         if (existingUser) {
           this.setState({ user: existingUser });
           this.props.setUser(true);
+          this.getGoalCount(uid);
+          this.setUserFeed(uid);
         }
       }).catch((err) => console.error('error getting user by uid from Home', err));
   }
 
   render() {
     const buildHome = () => {
-      const { user } = this.state;
+      const { user, goalsMet } = this.state;
       if (!user.id) {
         return (
           <div className='newUserHome'>
@@ -40,7 +62,15 @@ class Home extends React.Component {
       }
       return (
         <div className='currentUserHome'>
-          <h1> Welcome, {user.name}!</h1>
+          <div className='profileDisplay d-flex justify-content-end'>
+            <div className='pt-4'>
+              <p>Member Since {user.memberSince}</p>
+              <p>{goalsMet} Goals Met</p>
+            </div>
+            <img className='profilePic' src={user.imageUrl} alt={user.name} />
+          </div>
+          <h1> Welcome Back, {user.name}!</h1>
+          <h2>History</h2>
         </div>
       );
     };
--- src/helpers/data/goalData.js
+++ src/helpers/data/goalData.js
@@ -3,6 +3,21 @@ import firebaseConfig from '../apiKeys.json';
 
 const baseUrl = firebaseConfig.firebaseKeys.databaseURL;
 
+const getGoalsByUid = (uid) => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/goals.json?orderBy="uid"&equalTo="${uid}"`)
+    .then((response) => {
+      const goals = response.data;
+      const goalArr = [];
+      if (goals !== null) {
+        Object.keys(goals).forEach((fbId) => {
+          goals[fbId].id = fbId;
+          goalArr.push(goals[fbId]);
+        });
+      }
+      resolve(goalArr);
+    }).catch((err) => reject(err));
+});
+
 const createGoal = (goalObj) => axios.post(`${baseUrl}/goals.json`, goalObj);
 
-export default { createGoal };
+export default { createGoal, getGoalsByUid };
