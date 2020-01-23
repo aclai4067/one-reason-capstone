e3801888e2e536d3ed783e36f4e3c5839c1f9b2b
Ashley Claiborne <languageisaliquid@gmail.com>
2020-01-22 21:05:23 -0600

    creating goals state and passing props down to single goal

--- src/components/pages/Goals/Goals.js
+++ src/components/pages/Goals/Goals.js
@@ -1,15 +1,35 @@
 import './Goals.scss';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import goalData from '../../../helpers/data/goalData';
+import authData from '../../../helpers/data/authData';
+import SingleGoal from '../../shared/SingleGoal/SingleGoal';
 
 class Goals extends React.Component {
+  state = {
+    goals: [],
+  }
+
+  setGoals = (uid) => {
+    goalData.getGoalsByUid(uid)
+      .then((results) => {
+        this.setState({ goals: results });
+      }).catch((err) => console.error('error from setGoals', err));
+  }
+
+  componentDidMount() {
+    const uid = authData.getUid();
+    this.setGoals(uid);
+  }
+
   render() {
-    const goalId = 4067;
+    const { goals } = this.state;
+    const buildGoals = goals.map((goal) => <SingleGoal key={goal.id} goal={goal} />);
     return (
       <div className='Goals'>
         <h1>Goals</h1>
         <Link className='btn btn-outline-dark' to='goals/new'>New Goal</Link>
-        <Link className='btn btn-outline-info' to={`/goals/${goalId}/edit`}>Edit Goal</Link>
+        { buildGoals }
       </div>
     );
   }
---
+++ src/components/shared/SingleGoal/SingleGoal.js
@@ -0,0 +1,12 @@
+import './SingleGoal.scss';
+import React from 'react';
+
+class SingleGoal extends React.Component {
+  render() {
+    return (
+      <div className='SingleGoal'>Goal</div>
+    );
+  }
+}
+
+export default SingleGoal;
