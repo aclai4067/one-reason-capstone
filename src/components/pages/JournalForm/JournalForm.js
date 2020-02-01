1519ae159c64bdb29e103e734e17d4f238efe2ba
Ashley Claiborne <languageisaliquid@gmail.com>
2020-01-31 23:43:01 -0600

    validation works on journal form

--- src/components/pages/JournalForm/JournalForm.js
+++ src/components/pages/JournalForm/JournalForm.js
@@ -11,6 +11,8 @@ class JournalForm extends React.Component {
     journalGoalId: 'goal0',
     journalDate: '',
     userGoals: [],
+    titleValid: true,
+    bodyValid: true,
   }
 
   componentDidMount() {
@@ -36,45 +38,70 @@ class JournalForm extends React.Component {
 
   saveJournalEvent = (e) => {
     e.preventDefault();
-    const uid = authData.getUid();
-    const journalObj = {
-      date: new Date(),
-      title: this.state.journalTitle,
-      body: this.state.journalBody,
-      goalId: this.state.journalGoalId,
-      uid,
-    };
-    journalData.createJournal(journalObj)
-      .then(() => {
-        this.props.history.push('/journal');
-      }).catch((err) => console.error('error from saveFournalEvent', err));
+    const { journalTitle, journalBody } = this.state;
+    if (journalTitle.length > 0 && journalBody.length > 0) {
+      const uid = authData.getUid();
+      const journalObj = {
+        date: new Date(),
+        title: this.state.journalTitle,
+        body: this.state.journalBody,
+        goalId: this.state.journalGoalId,
+        uid,
+      };
+      journalData.createJournal(journalObj)
+        .then(() => {
+          this.props.history.push('/journal');
+        }).catch((err) => console.error('error from saveFournalEvent', err));
+    }
+    if (journalTitle.length < 1 && journalBody.length < 1) {
+      this.setState({ titleValid: false, bodyValid: false });
+    }
+    if (journalTitle.length < 1) {
+      this.setState({ titleValid: false });
+    }
+    if (journalBody.length < 1) {
+      this.setState({ bodyValid: false });
+    }
   }
 
   editJournalEvent = (e) => {
     e.preventDefault();
-    const uid = authData.getUid();
-    const { journalId } = this.props.match.params;
-    const journalObj = {
-      date: this.state.journalDate,
-      title: this.state.journalTitle,
-      body: this.state.journalBody,
-      goalId: this.state.journalGoalId,
-      uid,
-    };
-    journalData.changeJournal(journalId, journalObj)
-      .then(() => {
-        this.props.history.push('/journal');
-      }).catch((err) => console.error('error from editJournalEvent', err));
+    const { journalTitle, journalBody } = this.state;
+    if (journalTitle.length > 0 && journalBody.length > 0) {
+      const uid = authData.getUid();
+      const { journalId } = this.props.match.params;
+      const journalObj = {
+        date: this.state.journalDate,
+        title: journalTitle,
+        body: journalBody,
+        goalId: this.state.journalGoalId,
+        uid,
+      };
+      journalData.changeJournal(journalId, journalObj)
+        .then(() => {
+          this.props.history.push('/journal');
+        }).catch((err) => console.error('error from editJournalEvent', err));
+    }
   }
 
   changeTitle = (e) => {
     e.preventDefault();
-    this.setState({ journalTitle: e.target.value });
+    if (e.target.value.length < 1) {
+      this.setState({ journalTitle: e.target.value, titleValid: false });
+    }
+    if (e.target.value.length > 0) {
+      this.setState({ journalTitle: e.target.value, titleValid: true });
+    }
   }
 
   changeBody = (e) => {
     e.preventDefault();
-    this.setState({ journalBody: e.target.value });
+    if (e.target.value.length < 1) {
+      this.setState({ journalBody: e.target.value, bodyValid: false });
+    }
+    if (e.target.value.length > 0) {
+      this.setState({ journalBody: e.target.value, bodyValid: true });
+    }
   }
 
   changeGoalId = (e) => {
@@ -82,12 +109,22 @@ class JournalForm extends React.Component {
     this.setState({ journalGoalId: e.target.value });
   }
 
+  flagRequired = () => {
+    const { titleValid, bodyValid } = this.state;
+    if (!titleValid || !bodyValid) {
+      return (<p className='validationError'>Please Complete Required Fields</p>);
+    }
+    return ('');
+  };
+
   render() {
     const {
       journalTitle,
       journalBody,
       journalGoalId,
       userGoals,
+      titleValid,
+      bodyValid,
     } = this.state;
     const { journalId } = this.props.match.params;
     const buildGoalDropDown = userGoals.map((goal) => <option key={goal.id} value={goal.id}>{goal.name}</option>);
@@ -98,7 +135,7 @@ class JournalForm extends React.Component {
         <form className='col-sm-10 offset-sm-1'>
           <div className='form-group col-sm-8 offset-sm-2'>
             <label htmlFor='journalTitle'>Title</label>
-            <input type='text' className='form-control' id='journalTitle' value={journalTitle} onChange={this.changeTitle} placeholder='Title for journal entry' />
+            <input type='text' className={`form-control valid-${titleValid}`} id='journalTitle' value={journalTitle} onChange={this.changeTitle} placeholder='Title for journal entry' />
           </div>
           <div className='form-group col-sm-8 offset-sm-2'>
             <label htmlFor='journalGoalSelection' className='journalGoalDropdownLabel'>Related Goal</label>
@@ -109,9 +146,10 @@ class JournalForm extends React.Component {
           </div>
           <div className='form-group'>
             <label htmlFor='journalBody'>Body</label>
-            <textarea className='form-control journalBody' id='journalBody' value={journalBody} onChange={this.changeBody} placeholder='Journal your thoughts. This post will always be private.'>
+            <textarea className={`form-control journalBody valid-${bodyValid}`} id='journalBody' value={journalBody} onChange={this.changeBody} placeholder='Journal your thoughts. This post will always be private.'>
             </textarea>
           </div>
+          {this.flagRequired()}
           {
             (journalId) ? <button className='btn updateJournalBtn' onClick={this.editJournalEvent}>Update</button>
               : <button className='btn saveJournalBtn' onClick={this.saveJournalEvent}>Save</button>
